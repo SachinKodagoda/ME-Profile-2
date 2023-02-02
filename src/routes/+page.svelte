4259e9ac8bcbbb13abab3db5abfd6bae9aff2dae
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-02-02 10:49:44 +0530

    clean ups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,86 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import logo from '$lib/images/dk-logo.svg';
+  import './normalize.css';
+  import './styles.css';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="app">
+  <header class="header">
+    <div class="logo-ctr">
+      <img src={logo} alt="DK" class="logo-img" />
+    </div>
+    <nav class="nav">
+      <span aria-current="page" class="nav-items">About</span>
+      <span class="nav-items">Skills</span>
+      <span class="nav-items">Services</span>
+      <span class="nav-items">Contact</span>
+    </nav>
+  </header>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-	h1 {
-		width: 100%;
-	}
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 16px;
+    padding: 0 24px;
+    height: 60px;
+  }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+  .logo-ctr {
+    width: 3em;
+    height: 3em;
+  }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  .logo-img {
+    width: 2em;
+    height: 2em;
+    object-fit: contain;
+  }
+
+  .nav {
+    display: flex;
+    justify-content: flex-end;
+    gap: 16px;
+    font-size: 40px;
+    flex: auto;
+  }
+
+  .nav-items {
+    position: relative;
+    cursor: pointer;
+    display: none;
+    font-weight: 100;
+  }
+  .nav-items[aria-current='page'] {
+    font-weight: 700;
+  }
+  .nav-items[aria-current='page']::after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    border-radius: 6px;
+    background: #111;
+    position: absolute;
+    left: 0;
+    bottom: 0.2em;
+  }
+
+  @media only screen and (min-width: 769px) {
+    .nav-items {
+      display: block;
+    }
+  }
 </style>
