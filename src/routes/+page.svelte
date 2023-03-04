ae9d99776dcaf3135dc3739c7a46e03b27ecd305
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-03-05 00:01:06 +0530

    circle removed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -210,7 +210,38 @@
     <section class="about_section" style={`background-image: url(${back})`}>
       <!-- 🔥🔥🔥 AboutSection-AboutRing 🔥🔥🔥 -->
       <div class="about_ring about_ring_right" />
-      <div class="about_ring about_ring_left" />
+      <div class="about_ring about_ring_left">
+        <svg
+          class="about_cross_left"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="12" y1="5" x2="12" y2="19" />
+          <line x1="5" y1="12" x2="19" y2="12" />
+        </svg>
+      </div>
+      <svg
+        class="about_cross_top"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <line x1="12" y1="5" x2="12" y2="19" />
+        <line x1="5" y1="12" x2="19" y2="12" />
+      </svg>
       <svg
         class="about_cross_right"
         xmlns="http://www.w3.org/2000/svg"
